b75ee86491173465ac31256f8f9dfbf03c4721c7
Marhi03 <marhi.patel@gmail.com>
2025-08-21 12:28:51 +0530

    Add files via upload

---
+++ CS194_if1.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    /* This program prints largest and smallest nos."*/
+    int a, b, c;
+    printf("Enter 3 values.");
+    scanf("%d%d%d", &a, &b, &c);
+    if(a==b==c)
+         printf("All values are equal.\n");
+    else
+    {
+    if (a>=b && a>=c)
+          printf("%d is largest.\n", a);
+    else
+    if (b>=a && b>=c)
+          printf("%d is largest.\n", b);
+    else
+          printf("%d is largest.\n", c);
+
+    if (a<=b && a<=c)
+          printf("%d is smallest.\n", a);
+    else
+    if (b<=a && b<=c)
+          printf("%d is smallest.\n", b);
+    else
+          printf("%d is smallest.\n", c);
+    }
+}
