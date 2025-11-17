171929119fb6fa75835813bc2146c64b06fe987e
Marhi03 <marhi.patel@gmail.com>
2025-11-17 06:15:11 +0530

    Update CS230_func2.cpp

--- CS230_func2.cpp
+++ CS230_func2.cpp
@@ -11,7 +11,11 @@ int main()
 void leapyear(int x)
 {
     if(x%4 == 0)
-        printf("%d is a leap year.\n", x);
-    else
+    {
+        if(x%100 != 0)
+        {printf("%d is a leap year.\n", x);}
+        else
         printf("%d is not a leap year.\n", x);
+    }
 }
+
